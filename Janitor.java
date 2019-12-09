e649efa9f43162df5a2185c8761d19ce7233c043
SabraMGrace <sabramgrace@gmail.com>
2019-12-09 01:01:57 -0800

    Update Janitor.java

--- Janitor.java
+++ Janitor.java
@@ -1,7 +1,7 @@
 //*******************************************************************
 // ITC 115 (Fall 2019)
 // Employee.java
-
+/*
 Write a class Janitor to accompany the other law firm classes described
 in this chapter. Janitors work twice as many hours per week as other
 employees (80 hours/week), they make $30,000 ($10,000 less than
@@ -9,7 +9,7 @@ general employees), they get half as much vacation as other employees
 (only 5 days), and they have an additional method clean that prints
 "Workin' for the man." Make sure to interact with the superclass as
 Appropriate.
-
+*/
 // By: Sabra Grace
 //*******************************************************************
 
